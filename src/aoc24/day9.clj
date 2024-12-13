624a5f05337734eaffd7171aa8a66f63e98fe1b0
stephenallison <steve.allison@clara.co.uk>
2024-12-13 23:05:11 +0000

    Day 9 - use sorted map for free positions

--- src/aoc24/day9.clj
+++ src/aoc24/day9.clj
@@ -7,17 +7,17 @@
 ; output: map of files {positon: file-id}
 ;         list of free spaces [a b c]
 ;
-(defn new-scan
+(defn new-scan []
   {:file-positions  (sorted-map)
-   :free-positions  []
+   :free-positions  (sorted-map)
    :current-index   0
-   :current-file-id 0}
+   :current-file-id 0})
 
-  (defn update-scan-files [res updated n]
-    {:file-positions  updated
-     :free-positions  (:free-positions res)
-     :current-index   (+ (:current-index res) n)
-     :current-file-id (inc (:current-file-id res))}))
+(defn update-scan-files [res updated n]
+  {:file-positions  updated
+   :free-positions  (:free-positions res)
+   :current-index   (+ (:current-index res) n)
+   :current-file-id (inc (:current-file-id res))})
 
 (defn update-scan-free [res updated n]
   {:file-positions  (:file-positions res)
@@ -26,18 +26,18 @@
    :current-file-id (:current-file-id res)})
 
 (defn update-file-positions [res ch]
-  (let [file-positions (:file-positions res)
-        file-id (:current-file-id res)
+  (let [{file-positions :file-positions
+         file-id :current-file-id
+         output-idx :current-index} res
         n (edn/read-string (str ch))
-        output-idx (:current-index res)
         updated (into file-positions (map vector (range output-idx (+ output-idx n)) (repeat n file-id)))]
     (update-scan-files res updated n)))
 
 (defn update-free [res ch]
-  (let [free-positions (:free-positions res)
+  (let [{free-positions :free-positions
+         output-idx :current-index} res
         n (edn/read-string (str ch))
-        output-idx (:current-index res)
-        updated (into free-positions (range output-idx (+ output-idx n)))]
+        updated (into free-positions (map vector (range output-idx (+ output-idx n)) (repeat n \.)))]
     (update-scan-free res updated n)))
 
 (defn scan [res [index char]]
@@ -59,7 +59,7 @@
   (let [file-map (:file-positions res)
         free-pos (:free-positions res)
         moves (take-while (fn [[a b]] (> a b))
-                         (map vector (reverse (keys file-map)) free-pos))]
+                         (map vector (reverse (keys file-map)) (keys free-pos)))]
       (reduce move file-map moves)))
 
 (defn checksum [compressed]
