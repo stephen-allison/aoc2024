d0f944ea253c9bbe750a55a259c78cb10c4780a8
stephenallison <steve.allison@clara.co.uk>
2024-12-13 22:50:45 +0000

    Day 9 part 1

--- src/aoc24/day9.clj
+++ src/aoc24/day9.clj
@@ -2,60 +2,75 @@
   (:require [aoc24.loader :as loader])
   (:require [clojure.edn :as edn]))
 
-(defn process
-  ([[step-no value]]
-   (process step-no value))
-  ([step-no value]
-   (let [repeat-count (edn/read-string (str value))]
-     (if (even? step-no)
-       (apply str (repeat repeat-count (str (/ step-no 2))))
-       (apply str (repeat repeat-count "."))))))
+;
+; input: string 2333133121414131402
+;
+; output: map of files {positon: file-id}
+;         list of free spaces [a b c]
+;
+(defn scan-data
+  ([]
+   {:file-positions  (sorted-map)
+    :free-positions  []
+    :current-index   0
+    :current-file-id 0})
+  )
+
+(defn update-scan-files [res updated n]
+  {:file-positions  updated
+   :free-positions  (:free-positions res)
+   :current-index   (+ (:current-index res) n)
+   :current-file-id (inc (:current-file-id res))})
+
+(defn update-scan-free [res updated n]
+  {:file-positions  (:file-positions res)
+   :free-positions  updated
+   :current-index   (+ (:current-index res) n)
+   :current-file-id (:current-file-id res)})
+
+(defn update-file-positions [res input-idx ch]
+  (let [file-positions (:file-positions res)
+        file-id (:current-file-id res)
+        n (edn/read-string (str ch))
+        output-idx (:current-index res)
+        updated (into file-positions (map vector (range output-idx (+ output-idx n)) (repeat n file-id)))]
+    (update-scan-files res updated n)))
+
+(defn update-free [res idx ch]
+  (let [free-positions (:free-positions res)
+        n (edn/read-string (str ch))
+        output-idx (:current-index res)
+        updated (into free-positions (range output-idx (+ output-idx n)))]
+    (update-scan-free res updated n)))
+
+(defn scan [res [index char]]
+  (if (even? index)
+    (update-file-positions res index char)
+    (update-free res index char)))
 
 (defn expand-diskmap [diskmap]
-  (let [zipped (map-indexed vector diskmap)]
-    (apply str (map process zipped))))
+  (reduce scan (scan-data) (map-indexed vector diskmap)))
 
-(defn is-digit? [c]
-  (Character/isDigit c))
+(defn move
+  ([mapping from-index to-index]
+    (let [value (get mapping from-index)]
+      (dissoc (assoc mapping to-index value) from-index)))
+  ([mapping [from-index to-index]]
+    (move mapping from-index to-index)))
 
-(defn digit-locations [s]
-  (->> (map-indexed vector s)
-       (filter #(is-digit? (second %)))
-       (into [])))
+(defn compress [res]
+  (let [file-map (:file-positions res)
+        free-pos (:free-positions res)
+        moves (take-while (fn [[a b]] (> a b))
+                         (map vector (reverse (keys file-map)) free-pos))]
+      (reduce move file-map moves)))
 
-(defn dot-locations [s]
-  (->> (map-indexed vector s)
-       (filter #(= \. (second %)))
-       (map first)
-       reverse
-       (into [])))
-
-(defn compress [diskmap]
-  (let [digit-locs (digit-locations diskmap)
-        free-locs (dot-locations diskmap)
-        diskmap-transient! (transient (apply vector diskmap))]
-    (loop [digit-locs digit-locs
-           free-locs free-locs
-           diskmap! diskmap-transient!]
-      (let
-        [[digit-index digit] (peek digit-locs)
-         free-index (peek free-locs)]
-        (if (> free-index digit-index)
-          (persistent! diskmap!)
-          (recur (pop digit-locs)
-                 (pop free-locs)
-                 (assoc! (assoc! diskmap! free-index digit) digit-index \.)))))))
-
-(defn checksum [diskmap]
-  (->> (second (re-matches #"(\d+)\.*" diskmap))
-       (map-indexed vector)
-       (map (fn [[idx num]] (* idx (edn/read-string (str num)))))
-       (reduce +)))
+(defn checksum [compressed]
+  (reduce-kv (fn [acc k v] (+ acc (* k v))) 0 compressed))
 
 (defn solve [diskmap]
   (->> diskmap
        expand-diskmap
-       (apply vector)
        compress
-       ;checksum
+       checksum
        ))
